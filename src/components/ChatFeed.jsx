6e7ddeba83177a45db00f6c10bdfaa74e0ebcb19
Ritam Mahato <ritammahato15@gmail.com>
2021-08-25 21:50:25 +0530

    space

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -4,7 +4,7 @@ import MessageForm from './MessageForm';
 
 const ChatFeed = (props) => {
   const { chats, activeChat, userName, messages } = props;
-
+  
   const chat = chats && chats[activeChat];
 
   const renderReadReceipts = (message, isMyMessage) => chat.people.map((person, index) => person.last_read === message.id && (
